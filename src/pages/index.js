34023716290642242a7103bf69f1a11030e9bb73
rodolph66 <rschwenzner@gmail.com>
2020-10-15 09:26:35 +0300

    added styles and modified readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <div>
+    <h2>Dukkan Al Basha Demo Page</h2>
+    <p>A demo site is currently under construction using Gastby</p>
+    <p>Currently testing the automation of site content generation and upload from github using buddy</p>
+  </div>
+  )
 }
